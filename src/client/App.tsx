f368fdca869b2f36006643c54e305c56820c2ea5
James Fogarty <jayfo@users.noreply.github.com>
2022-06-01 13:19:13 -0700

    Stub and Link Project Pages

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -27,6 +27,22 @@ import Calendar from "src/pages/Calendar.mdx";
 import Home from "src/pages/Home.mdx";
 import Projects from "src/pages/Projects.mdx";
 
+import ProjectBloom from "src/projects/bloom/project_post.mdx";
+import ProjectBuzzed from "src/projects/buzzed/project_post.mdx";
+import ProjectClark from "src/projects/clark/project_post.mdx";
+import ProjectClosr from "src/projects/closr/project_post.mdx";
+import ProjectCupanion from "src/projects/cupanion/project_post.mdx";
+import ProjectDispurrse from "src/projects/dispurrse/project_post.mdx";
+import ProjectEasyList from "src/projects/easylist/project_post.mdx";
+import ProjectFoClass from "src/projects/foclass/project_post.mdx";
+import ProjectQuik from "src/projects/quik/project_post.mdx";
+import ProjectReci from "src/projects/reci/project_post.mdx";
+import ProjectRecoverTogether from "src/projects/recovertogether/project_post.mdx";
+import ProjectSousChef from "src/projects/souschef/project_post.mdx";
+import ProjectSprout from "src/projects/sprout/project_post.mdx";
+import ProjectStackBased from "src/projects/stackbased/project_post.mdx";
+import ProjectWasteWizard from "src/projects/wastewizard/project_post.mdx";
+
 import AssignmentsOverviewWrapper from "src/content/assignments/OverviewWrapper.mdx"
 import Assignment0 from "src/content/assignments/assignment0/Assignment0.mdx"
 import Assignment1 from "src/content/assignments/assignment1/Assignment1.mdx"
@@ -157,6 +173,21 @@ export const App: React.FunctionComponent = () => {
                     <Routes>
                         <Route path="/" element={<Home/>} />
                         <Route path="/projects" element={<Projects/>} />
+                        <Route path="/projects/bloom" element={<ProjectBloom/>} />
+                        <Route path="/projects/buzzed" element={<ProjectBuzzed/>} />
+                        <Route path="/projects/clark" element={<ProjectClark/>} />
+                        <Route path="/projects/closr" element={<ProjectClosr/>} />
+                        <Route path="/projects/cupanion" element={<ProjectCupanion/>} />
+                        <Route path="/projects/dispurrse" element={<ProjectDispurrse/>} />
+                        <Route path="/projects/easylist" element={<ProjectEasyList/>} />
+                        <Route path="/projects/foclass" element={<ProjectFoClass/>} />
+                        <Route path="/projects/quik" element={<ProjectQuik/>} />
+                        <Route path="/projects/reci" element={<ProjectReci/>} />
+                        <Route path="/projects/recovertogether" element={<ProjectRecoverTogether/>} />
+                        <Route path="/projects/souschef" element={<ProjectSousChef/>} />
+                        <Route path="/projects/sprout" element={<ProjectSprout/>} />
+                        <Route path="/projects/stackbased" element={<ProjectStackBased/>} />
+                        <Route path="/projects/wastewizard" element={<ProjectWasteWizard/>} />
                         <Route path="/assignments" element={<Assignments/>}>
                             <Route path="" element={<AssignmentsOverviewWrapper/>} />
                             <Route path="assignment0" element={<Assignment0/>} />
