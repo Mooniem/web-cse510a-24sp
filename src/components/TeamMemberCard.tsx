154e8e4e8e6e1dabe7f412c2c9af4f8236c028cf
Anant Mittal <anmittal@cs.washington.edu>
2022-06-02 11:25:38 -0700

    Added TeamMemberCard component

---
+++ src/components/TeamMemberCard.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { FunctionComponent } from 'react';
+
+import {
+    Card,
+    CardContent,
+    Stack,
+} from '@mui/material';
+
+interface TeamMemberProps {
+    name: string;
+    photo: string;
+}
+
+export const TeamMemberCard: FunctionComponent<TeamMemberProps> = (props) => {
+    return (
+        <Card>
+            <CardContent>
+                <Stack>
+                    { props.name }
+                    <img
+                        src={props.photo}
+                        height="150"
+                        width="150"
+                        alt={"Photo of " + props.name }
+                    />
+                </Stack>
+            </CardContent>
+        </Card>
+    );
+}
+
+export default TeamMemberCard;
